613b6d2227ccd5eaf7397e0dc7ab139aa240ff5e
Jérémy <3dx900@gmail.com>
2022-04-23 21:49:57 +0200

    Add Commercial schema

---
+++ server/app/models/commercial.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CommercialSchema = new Schema({
+  recetteGlobale: {
+    type: Number,
+  },
+  nbVentes: {
+    type: [
+      {
+        label: String,
+        value: Number,
+      },
+    ],
+  },
+  nbKilometres: {
+    type: [
+      {
+        label: String,
+        value: Number,
+      },
+    ],
+  },
+  recetteCommercial: {
+    type: [
+      {
+        label: String,
+        value: Number,
+      },
+    ],
+  },
+  nbReclamations: {
+    type: [
+      {
+        label: String,
+        value: Number,
+      },
+    ],
+  },
+});
+
+module.exports = mongoose.model("Commercial", CommercialSchema);
