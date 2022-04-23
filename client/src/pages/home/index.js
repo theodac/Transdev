73547ed5f7db519abe2ccb1881ada3a2b2a20f56
maxencelgy <maxencelgy@gmail.com>
2022-04-23 16:55:29 +0200

    create side bar & hover

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,9 +1,36 @@
 import React from "react";
-
+import "../../styles/pages/home.scss";
+import BasicCard from "../../components/basicCard";
 const HomePage = () => {
+  function appear() {
+    const sideBar = document.querySelector(".scrollBar");
+    sideBar.classList.add("active");
+  }
+  function disappear() {
+    const sideBar = document.querySelector(".scrollBar");
+    sideBar.classList.remove("active");
+  }
+
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="wrap">
+      <div className="left">
+        <div className="top">
+          <p className="parent" onMouseOver={appear} onMouseLeave={disappear}>
+            home
+            <BasicCard bgColor={"#333333"} />
+          </p>
+          <p onMouseOver={appear} onMouseLeave={disappear}>
+            home2
+            <BasicCard />
+          </p>
+          <p onMouseOver={appear} onMouseLeave={disappear}>
+            home3
+            <BasicCard />
+          </p>
+        </div>
+        <div className="bottom"></div>
+      </div>
+      <div className="right"></div>
     </div>
   );
 };
