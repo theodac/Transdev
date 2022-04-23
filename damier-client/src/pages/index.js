1b6fc17adf0802f60b0e00b769081b162e778d27
Jérémy <3dx900@gmail.com>
2022-04-23 12:34:52 +0200

    Add routing system and home page component

--- damier-client/src/pages/index.js
+++ damier-client/src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import HomePage from "./home";
+
 const Root = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" component={HomePage} />
+        <Route exact path="/" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
