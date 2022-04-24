927506931b80f5d538d9e50cc2dbf231c6338ae7
novaedra <novaedra@gmail.com>
2022-04-24 06:43:41 +0200

    fix shit send help please i need sleep

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 import HomePage from "./home";
+import Single from "./home/single";
 
 const Root = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<HomePage/>}/>
+                <Route exact path="/single" element={<Single/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default Root;
