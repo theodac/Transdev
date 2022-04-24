5fa3fc2545253a97db5bf3a4bb1f8a53f7b16d4e
Jérémy <3dx900@gmail.com>
2022-04-24 10:28:15 +0200

    Fix Sidebar

--- client/src/pages/home/achat.js
+++ client/src/pages/home/achat.js
@@ -1,60 +1,60 @@
 import React from "react";
 import "../../styles/pages/achat.scss";
 
-
 const Achat = () => {
-
-
-    return (
-
-        <div id="achat">
-
-
-            <div className="wrap_single">
-                <div className="cube1"></div>
-                <div className="cube2">
-                    <div className="arrow_top"></div>
-                    <p className="commerciale">Achat</p>
-                    <h2>commande passées</h2>
-                    <p className="number">148 634</p>
-                    <button className="btn-voir">en voir plus...</button>
-                </div>
-                <div className="cube3"></div>
-                <div className="cube4">
-                    <div className="arrow_left"></div>
-                    <p className="technique">Technique</p>
-                    <h2>Statistiques 2</h2>
-                    <div className="text_damier"><p>Lorem ipsum dolor sit amet, consectetur adipising elit. consectetur
-                        adipising elit. </p>
-                        <button className="btn-voir">en voir plus...</button>
-                    </div>
-                </div>
-                <div className="cube5">
-                    <h2>Date : </h2>
-                    <input type="date" id="start" name="trip-start"/>
-                </div>
-                <div className="cube6">
-                    <div className="arrow_right"></div>
-                    <p className="exploitation">Exploitation</p>
-                    <h2>Statistiques 2</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipising elit. consectetur adipising elit. </p>
-                    <button className="btn-voir">en voir plus...</button>
-                </div>
-                <div className="cube7"></div>
-                <div className="cube8">
-                    <div className="arrow_down"></div>
-                    <p className="ressources">Ressources humaines</p>
-                    <h2>Statistiques 2</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipising elit. consectetur adipising elit. </p>
-                    <button className="btn-voir">en voir plus...</button>
-                </div>
-                <div className="cube9"></div>
-
-
-            </div>
+  return (
+    <div id="achat">
+      <div className="wrap_single">
+        <div className="cube1"></div>
+        <div className="cube2">
+          <div className="arrow_top"></div>
+          <p className="commerciale">Achat</p>
+          <h2>commande passées</h2>
+          <p className="number">148 634</p>
+          <button className="btn-voir">en voir plus...</button>
         </div>
-
-    );
+        <div className="cube3"></div>
+        <div className="cube4">
+          <div className="arrow_left"></div>
+          <p className="technique">Technique</p>
+          <h2>Statistiques 2</h2>
+          <div className="text_damier">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipising elit.
+              consectetur adipising elit.{" "}
+            </p>
+            <button className="btn-voir">en voir plus...</button>
+          </div>
+        </div>
+        <div className="cube5">
+          <h2>Date : </h2>
+          <input type="date" id="start" name="trip-start" />
+        </div>
+        <div className="cube6">
+          <div className="arrow_right"></div>
+          <p className="exploitation">Exploitation</p>
+          <h2>Statistiques 2</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipising elit. consectetur
+            adipising elit.{" "}
+          </p>
+          <button className="btn-voir">en voir plus...</button>
+        </div>
+        <div className="cube7"></div>
+        <div className="cube8">
+          <div className="arrow_down"></div>
+          <p className="ressources">Ressources humaines</p>
+          <h2>Statistiques 2</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipising elit. consectetur
+            adipising elit.{" "}
+          </p>
+          <button className="btn-voir">en voir plus...</button>
+        </div>
+        <div className="cube9"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Achat;
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,22 +1,24 @@
 import React from "react";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import HomePage from "./home";
 import Single from "./home/single";
 import Commerciale from "./home/commerciale";
 import Achat from "./home/achat";
+import Sidebar from "../components/Siderbar";
 
 const Root = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route exact path="/" element={<HomePage/>}/>
-                <Route exact path="/single" element={<Single/>}/>
-                <Route exact path="/commerciale" element={<Commerciale/>}/>
-                <Route exact path="/achat" element={<Achat/>}/>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Sidebar />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/single" element={<Single />} />
+        <Route exact path="/commerciale" element={<Commerciale />} />
+        <Route exact path="/achat" element={<Achat />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Root;
