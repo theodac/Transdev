7f2db28ebab78ee679f1f3675eb42d33f9553ab0
Jérémy <3dx900@gmail.com>
2022-04-24 12:55:41 +0200

    Fix conflicts

--- client/src/pages/home/single.js
+++ client/src/pages/home/single.js
@@ -2,56 +2,57 @@ import React from "react";
 import "../../styles/pages/single.scss";
 
 const Single = () => {
-
-
-    return (
-
-        <>
-
-            <div id="single">
-            <div className="wrap_single" >
-                <div className="cude_2d_1"></div>
-                <div className="cude_2d_2">
-                    <div className="arrow_top"></div>
-                    <p className="commerciale">Commerciale</p>
-                    <h2>commande passées</h2>
-                    <p className="number">148 634</p>
-                    <button className="btn-voir">en voir plus...</button>
-                </div>
-                <div className="cude_2d_3"></div>
-                <div className="cude_2d_4">
-                    <div className="arrow_left"></div>
-                    <p className="technique">Technique</p>
-                    <h2>Statistiques 2</h2>
-                    <div className="text_damier"><p>Lorem ipsum dolor sit amet, consectetur adipising elit. consectetur
-                        adipising elit. </p>
-                        <button className="btn-voir">en voir plus...</button>
-                    </div>
-                </div>
-                <div className="cude_2d_5">
-                    <h2>Date : </h2>
-                    <input type="date" id="start" name="trip-start"/>
-                </div>
-                <div className="cude_2d_6">
-                    <div className="arrow_right"></div>
-                    <p className="exploitation">Exploitation</p>
-                    <h2>Statistiques 2</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipising elit. consectetur adipising elit. </p>
-                    <button className="btn-voir">en voir plus...</button>
-                </div>
-                <div className="cude_2d_7"></div>
-                <div className="cude_2d_8">
-                    <div className="arrow_down"></div>
-                    <p className="ressources">Ressources humaines</p>
-                    <h2>Statistiques 2</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipising elit. consectetur adipising elit. </p>
-                    <button className="btn-voir">en voir plus...</button>
-                </div>
-                <div className="cude_2d_9"></div>
-
-
+  return (
+    <>
+      <div id="single">
+        <div className="wrap_single">
+          <div className="cude_2d_1"></div>
+          <div className="cude_2d_2">
+            <div className="arrow_top"></div>
+            <p className="commerciale">Commerciale</p>
+            <h2>commande passées</h2>
+            <p className="number">148 634</p>
+            <button className="btn-voir">en voir plus...</button>
+          </div>
+          <div className="cude_2d_3"></div>
+          <div className="cude_2d_4">
+            <div className="arrow_left"></div>
+            <p className="technique">Technique</p>
+            <h2>Statistiques 2</h2>
+            <div className="text_damier">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipising elit.
+                consectetur adipising elit.{" "}
+              </p>
+              <button className="btn-voir">en voir plus...</button>
             </div>
           </div>
+          <div className="cude_2d_5">
+            <h2>Date : </h2>
+            <input type="date" id="start" name="trip-start" />
+          </div>
+          <div className="cude_2d_6">
+            <div className="arrow_right"></div>
+            <p className="exploitation">Exploitation</p>
+            <h2>Statistiques 2</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipising elit.
+              consectetur adipising elit.{" "}
+            </p>
+            <button className="btn-voir">en voir plus...</button>
+          </div>
+          <div className="cude_2d_7"></div>
+          <div className="cude_2d_8">
+            <div className="arrow_down"></div>
+            <p className="ressources">Ressources humaines</p>
+            <h2>Statistiques 2</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipising elit.
+              consectetur adipising elit.{" "}
+            </p>
+            <button className="btn-voir">en voir plus...</button>
+          </div>
+          <div className="cude_2d_9"></div>
           <div className="cube5">
             <h2>Date : </h2>
             <input type="date" id="start" name="trip-start" />
