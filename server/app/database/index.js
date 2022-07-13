8c47ed55506e9ddd02cfa73b453ba0467bb2f0c6
NielsBeaumont <nielsbeaumont.etu@gmail.com>
2022-07-13 11:44:01 +0200

    creation exploitation v2 pour angular

--- server/app/database/index.js
+++ server/app/database/index.js
@@ -19,8 +19,6 @@ const buildConnectionUri = () => {
   const { MONGO_USER, MONGO_PASSWORD, MONGO_HOST, MONGO_DB } = process.env;
 
   return `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DB}?retryWrites=true&w=majority`;
-
-  //return 'mongodb://localhost:27017/damier'
 };
 
 module.exports = { connectDatabase };
