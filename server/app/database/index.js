736709e7d5031b1fc543a565499c9e5ee69adf5c
BaptisteAngot <contact@angotbaptiste.com>
2022-07-13 14:28:28 +0200

    old mongo

--- server/app/database/index.js
+++ server/app/database/index.js
@@ -18,7 +18,8 @@ const connectDatabase = () => {
 const buildConnectionUri = () => {
   const { MONGO_USER, MONGO_PASSWORD, MONGO_HOST, MONGO_DB } = process.env;
 
-  return `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DB}?retryWrites=true&w=majority`;
+  // return `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DB}?retryWrites=true&w=majority`;
+  return 'mongodb://localhost:27017/damier'
 };
 
 module.exports = { connectDatabase };
